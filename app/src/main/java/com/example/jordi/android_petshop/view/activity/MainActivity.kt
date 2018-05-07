5d8991cf6b39c0732f532b9a7034664ef8d02edf
Jordi Rubiralta <jordi.rubiraltacortes@worldline.com>
2018-05-07 08:15:38 +0200

    MainActivity done

--- app/src/main/java/com/example/jordi/android_petshop/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/jordi/android_petshop/view/activity/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.example.jordi.android_petshop.view.activity
 
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.example.domain.model.GenreList
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.provider
 import com.example.jordi.android_petshop.R
-import com.example.jordi.android_petshop.presenter.MainPresenter
+import com.example.jordi.android_petshop.model.GenreListView
+import com.example.jordi.android_petshop.presenter.genre.GenreListPresenter
+import com.example.jordi.android_petshop.view.adapter.GenreAdapter
 
 
-class MainActivity : RootActivity<MainPresenter.View>(), MainPresenter.View {
+class MainActivity : RootActivity<GenreListPresenter.View>(), GenreListPresenter.View {
 
     override val activityModule: Kodein.Module = Kodein.Module {
-        bind<MainPresenter>() with provider {
-            MainPresenter(errorHandler = instance(), view = this@MainActivity)
+        bind<GenreListPresenter>() with provider {
+            GenreListPresenter(
+                    getGenreUseCase = instance(),
+                    errorHandler = instance(),
+                    view = this@MainActivity)
         }
     }
 
-    override val presenter: MainPresenter by instance()
+    override val presenter: GenreListPresenter by instance()
 
-    override val layoutResourceId: Int = R.layout.activity_main
+    private val genreListAdapter = GenreAdapter()
+
+    override val layoutResourceId: Int = R.layout.list_genre
+
+    val genreList: RecyclerView by lazy { genreList }
 
     override fun showProgress() {
         //throw RuntimeException(getString(R.string.progress_no_available))
@@ -29,7 +41,8 @@ class MainActivity : RootActivity<MainPresenter.View>(), MainPresenter.View {
     }
 
     override fun initializeUI() {
-
+        genreList.adapter = genreListAdapter
+        genreList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 
     override fun registerListeners() {
@@ -39,4 +52,7 @@ class MainActivity : RootActivity<MainPresenter.View>(), MainPresenter.View {
         super.onDestroy()
     }
 
+    override fun showGenreList(genreList: GenreListView) {
+        genreListAdapter.addAll(genreList.genresList)
+    }
 }
