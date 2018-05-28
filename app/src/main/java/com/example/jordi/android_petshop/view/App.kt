81164f6d7a45513c4383dddadb3c5c125828378b
Jordi Rubiralta <jordi.rubiraltacortes@worldline.com>
2018-05-28 10:57:54 +0200

    Added leakCanary

--- app/src/main/java/com/example/jordi/android_petshop/view/App.kt
+++ app/src/main/java/com/example/jordi/android_petshop/view/App.kt
@@ -1,12 +1,15 @@
 package com.example.jordi.android_petshop.view
 
 import android.app.Application
+import android.content.Context
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.KodeinAware
 import com.github.salomonbrys.kodein.lazy
 import com.example.jordi.android_petshop.di.appModule
 import com.example.jordi.android_petshop.di.dataModule
 import com.example.jordi.android_petshop.di.domainModule
+import com.squareup.leakcanary.LeakCanary
+import com.squareup.leakcanary.RefWatcher
 
 /**
  * App.
@@ -20,7 +23,18 @@ class App : Application(), KodeinAware {
         import(dataModule)
     }
 
+    fun getRefWatcher(context: Context): RefWatcher {
+        val application: App = context.applicationContext as App
+        return application.refWatcher
+    }
+
+    lateinit var refWatcher: RefWatcher
+
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
     }
 }
